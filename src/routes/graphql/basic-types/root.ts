7a93eedbc00003a9f06b32f7b4a053d2e2bc8315
Izy <126877709+izy-code@users.noreply.github.com>
2024-11-01 21:51:06 +0300

    feat: add post queries

--- src/routes/graphql/basic-types/root.ts
+++ src/routes/graphql/basic-types/root.ts
@@ -1,10 +1,13 @@
 import { GraphQLObjectType } from "graphql";
 import { memberTypeQuery, memberTypesQuery } from "../query-types/memberTypeQueries.js";
+import { postQuery, postsQuery } from "../query-types/postQueries.js";
 
 export const rootQueryType = new GraphQLObjectType({
   name: 'Query',
   fields: {
     memberTypes: memberTypesQuery,
     memberType: memberTypeQuery,
+    posts: postsQuery,
+    post: postQuery,
   },
 });
